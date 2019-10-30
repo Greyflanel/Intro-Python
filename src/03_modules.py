3cb81aaecac25c1fe26b9f38b159eda9dc39315c
Greyflanel <richardlovelace811@gmail.com>
2019-10-30 14:49:32 -0500

    finished modules.py which consists of printing arguments for sys.argv, the name of os, python version, process ID, CWD, and machine login name.

---
+++ src/03_modules.py
@@ -0,0 +1,13 @@
+import sys
+print('Length of sys.argv :', len(sys.argv))
+print('Argument(s)  :', str(sys.argv))
+import platform
+print('OS Platform  :', sys.platform)
+print('Python Version  :', platform.python_version())
+
+import os
+print('Current Process ID :', os.getpid())
+print('Current Working Directory  :', os.getcwd())
+
+import getpass
+print('Current login name  :', getpass.getuser())
