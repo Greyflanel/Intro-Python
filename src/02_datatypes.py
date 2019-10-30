627be7ce5ac2dace24e8fbdf14312808cc2deed4
Greyflanel <richardlovelace811@gmail.com>
2019-10-30 18:29:31 -0500

    finished 05 _lists.py which involved methods and functions of lists for adding on to, editing, removing and combining them

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -1,6 +1,11 @@
 x = 5
 y = "7"
 
+# Write a print statement that combines x + y into the integer value 12
+
 addTypes = int(y) + x
 print(addTypes)
+
+# Write a print statement that combines x + y into the string value 57
+
 print(str(x) + y)
